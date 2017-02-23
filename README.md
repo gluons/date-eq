ef4095406623dd27315239b39f8c9fa2a9724ba1
Saran Tanpituckpong <sarunta@gmail.com>
2017-02-23 09:35:36 +0700

    📜 docute!

---
+++ README.md
@@ -0,0 +1,6 @@
+# Date EQ
+GitHub pages documentation.
+
+Go to https://gluons.github.io/date-eq/
+
+Made with [📜 docute](https://github.com/egoist/docute).
---
+++ config.js
@@ -0,0 +1,15 @@
+self.$config = {
+	home: 'https://raw.githubusercontent.com/gluons/date-eq/master/README.md',
+	repo: 'gluons/date-eq',
+	nav: [
+		{ title: 'Home', path: '/' }
+	],
+	icons: [
+		{
+			label: 'Follow me on Google+',
+			svgId: 'google-plus',
+			svgClass: 'google-plus-icon',
+			link: 'https://www.google.com/+SaranTanpituckpong'
+		}
+	]
+};
